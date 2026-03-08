0e2ba891056a96657b468fe8ef1e2f96a69fb302
lxdklp <lxdklp@lxdklp.top>
2026-03-08 18:28:26 +0800

    init

---
+++ bin/fad.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+rm -rf /mnt/us/system/.assets/* \
+       /mnt/us/system/.mrch/* \
+       /var/local/adunits/* \
+       /var/local/merchant/*
+
+sqlite3 appreg.db <<'SQL'
+UPDATE properties
+   SET value='false'
+ WHERE handlerid='dcc'
+   AND name='dtcp_pref_ShowRecsPref';
+
+DELETE FROM properties
+ WHERE handlerid='dcc'
+   AND name IN (
+       'adunit.viewable',
+       'dtcp_pref_ShowScreensaverPref',
+       'dtcp_pref_ShowBannerPref'
+   );
+SQL
+
+reboot
